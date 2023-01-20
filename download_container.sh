c23107249241a51ab67087eab5655d03b08c4478
Luigi311 <git@luigi311.com>
2023-01-20 10:28:54 -0700

    Switch setup and download to posix sh

--- download_container.sh
+++ download_container.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 podman pull docker.io/luigi311/low-power-image-processing:latest
 podman image prune -f
--- setup.sh
+++ setup.sh
@@ -1,48 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
-
-setup_main() {
-    PASSED_USER="$1"
-
-    if [ ! -e /etc/nsswitch.conf ]; then
-        echo 'hosts: files dns' > /etc/nsswitch.conf
-    fi
-
-    # If /etc/subgid or /etc/subuid do not exist, create them
-    if [ ! -f /etc/subgid ]; then
-        touch /etc/subgid
-    fi
-
-    if [ ! -f /etc/subuid ]; then
-        touch /etc/subuid
-    fi
-
-    if ! command -v "podman" >/dev/null; then
-        if command -v "pacman" >/dev/null; then
-            pacman -Sy podman fuse-overlayfs --noconfirm
-        elif command -v "apk" >/dev/null; then
-            apk update
-            apk add podman fuse-overlayfs shadow slirp4netns
-            rc-update add cgroups
-            rc-service cgroups start
-            modprobe tun
-        else
-            echo "Unknown package manager, podman needs to be install via your preferred method"
-        fi
-    fi
-
-    # If $PASSED_USER is not in /etc/subgid or /etc/subuid
-    if ! grep -q "^${PASSED_USER}:100000" /etc/subgid || ! grep -q "^${PASSED_USER}:100000" /etc/subuid; then
-        chmod u+s /usr/bin/newuidmap
-        chmod u+s /usr/bin/newgidmap
-        echo "Adding ${PASSED_USER} to /etc/subgid and /etc/subuid"
-        usermod --add-subuids 100000-165535 --add-subgids 100000-165535 "${PASSED_USER}"
-        echo "User permisisons adjusted, reboot machine afterwards"
-    fi
-    
-    podman system migrate
-}
 
 
 mkdir -p "${HOME}/.config/megapixels"
@@ -53,13 +11,64 @@ ln -s "${PWD}/postprocess.sh" "${HOME}/.config/megapixels/postprocess.sh"
 
 chmod +x "${HOME}/.config/megapixels/postprocess.sh"
 
-export -f setup_main
-FUNC=$(declare -f setup_main)
-
-if [[ $EUID -ne 0 ]]; then
-    USER="$(whoami)"
-    sudo bash -c "$FUNC; setup_main $USER"
-else
+if [ "$(id -u)" -eq 0 ]; then
     echo "Script must not be ran as root"
     exit 1
 fi
+
+USER="$(whoami)"
+
+if [ -z "${USER}" ]; then
+    echo "Could not determine current user"
+    exit 1
+fi
+
+# Switch to root
+sudo -s <<EOF
+if [ ! -e /etc/nsswitch.conf ]; then
+    echo 'hosts: files dns' > /etc/nsswitch.conf
+fi
+
+# If /etc/subgid or /etc/subuid do not exist, create them
+if [ ! -f /etc/subgid ]; then
+    touch /etc/subgid
+fi
+
+if [ ! -f /etc/subuid ]; then
+    touch /etc/subuid
+fi
+
+if ! command -v "bash" > /dev/null; then
+    if command -v "pacman" >/dev/null; then
+        pacman -Sy bash
+    elif command -v "apk" >/dev/null; then
+        apk update
+        apk add bash
+    fi
+fi
+
+if ! command -v "podman" >/dev/null; then
+    if command -v "pacman" >/dev/null; then
+        pacman -Sy podman fuse-overlayfs --noconfirm
+    elif command -v "apk" >/dev/null; then
+        apk update
+        apk add podman fuse-overlayfs shadow slirp4netns
+        rc-update add cgroups
+        rc-service cgroups start
+        modprobe tun
+    else
+        echo "Unknown package manager, podman needs to be install via your preferred method"
+    fi
+fi
+
+# If $USER is not in /etc/subgid or /etc/subuid
+if ! grep -q "^${USER}:100000" /etc/subgid || ! grep -q "^${USER}:100000" /etc/subuid; then
+    chmod u+s /usr/bin/newuidmap
+    chmod u+s /usr/bin/newgidmap
+    echo "Adding ${USER} to /etc/subgid and /etc/subuid"
+    usermod --add-subuids 100000-165535 --add-subgids 100000-165535 "${USER}"
+    echo "User permisisons adjusted, reboot machine afterwards"
+fi
+
+podman system migrate
+EOF
